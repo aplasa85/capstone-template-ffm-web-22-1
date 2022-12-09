cef0b0f73f6c5917cef52e2a55f46c42eb97fe10
Axel Plasa <aplasa@gmail.com>
2022-12-09 10:50:22 +0100

    Create new page

---
+++ pages/products.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+function Products () {
+return (
+    
+<div>
+      <StyledTitle>Casa del Agave</StyledTitle>
+
+   </div>  
+    
+
+
+);
+
+}
+
+
+export default  Products;
+
+const StyledTitle = styled.h1`
+  border: 1px solid black;
+  width: 400px;
+  height: 150px;
+  padding: 55px;
+  padding-left: 75px;
+  margin-left: 32%;
+  margin-top: 70px;
+`;
