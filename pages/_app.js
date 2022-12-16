d128690e4fa152c2279d9c67e206d6b5912d356d
Axel Plasa <aplasa@gmail.com>
2022-12-16 13:18:26 +0100

    git fix 3rd attempt

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import GlobalStyles from "../styles/GlobalStyles";
-import Layout from "../components/Layout";
+import Layout from "../components/LayoutComponent";
 
 function MyApp({Component, pageProps}) {
   return (
