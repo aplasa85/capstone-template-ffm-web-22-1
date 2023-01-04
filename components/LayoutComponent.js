6c5f8f11c57fb2158e3f11e7580cafb4ff798a7a
Axel Plasa <aplasa@gmail.com>
2023-01-04 10:36:51 +0100

    Fixed bug

--- components/LayoutComponent.js
+++ components/LayoutComponent.js
@@ -1,55 +1,40 @@
-import React from "react";
-import Image from "next/image";
-import Icon from "../public/Assets/Agave.jpg";
 import styled from "styled-components";
-import Link from "next/link";
+import Navbar from "./Navbar";
 
-const Header = () => {
+export default function Layout({children}) {
   return (
-    <div>
-      <Link href="/">
-        <StyledTitle>Casa del Agave</StyledTitle>
-      </Link>
+    <StyledContainer>
+      <Navbar />
+      <StyledMainDiv>{children}</StyledMainDiv>
 
-      <StyledParagraph>
-        Welcome to the Casa del Agave online shop. Here you can find a fine
-        selection of tequila and mezcal brands delivered right to your home.
-        Thank you for choosing us and we certainly hope you will find the
-        beverage of your convenience.
-      </StyledParagraph>
-
-      <StyledImage src={Icon} width="310" height="255" alt="Agave Plant" />
-    </div>
+      <StyledFooter>
+        <p>© Casa del Agave</p>
+      </StyledFooter>
+    </StyledContainer>
   );
-};
+}
 
-export default Header;
-
-const StyledImage = styled(Image)`
-  position: relative;
-  left: 53.125rem;
-  bottom: 16.25rem;
-  border: 1px solid black;
-  padding: 1.875rem;
+const StyledMainDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: 88.2vh;
+  margin: 0 3%;
 `;
 
-const StyledTitle = styled.h1`
-  border: 1px solid black;
-  width: 25rem;
-  height: 9.375rem;
-  padding: 3.4rem;
-  padding-left: 4.7rem;
-  margin-left: 32%;
-  margin-top: 4.3rem;
+const StyledFooter = styled.footer`
+  background-color: grey;
+  color: white;
+  height: 60px;
+  padding: 15px;
+  padding-top: 8px;
+  width: 100%;
 `;
 
-const StyledParagraph = styled.p`
-  border: 1px solid black;
-  width: 18.75rem;
-  height: 14.7rem;
-  margin-top: 6.25rem;
-  margin-left: 9.375rem;
-  font-size: 1.25rem;
-  align-content: center;
-  padding: 0, 625rem;
+const StyledContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
 `;
